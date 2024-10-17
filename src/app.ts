4b23b5a1d24af5b0eb4456a95d14937da84bee74
Tiago Filho <tiagofilho.stz@gmail.com>
2024-10-17 16:44:59 -0300

    Alteração do app.ts, server.ts e router.ts de acordo com a Issue do GitHub

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,15 @@
-import express from "express";
-import cors from "cors";
+import { server } from './server';
 import { DatabaseModel } from './model/DatabaseModel';
-import { Cliente } from "./model/Cliente";
-import ClienteController from "./controller/ClienteController";
-import CarroController from "./controller/CarroController";
-import { Carro } from "./model/Carro";
-import PedidoVendaController from "./controller/PedidoVendaController";
-import { PedidoVenda } from "./model/PedidoVenda";
 
 const port: number = 3333;
 
-const server = express();
-server.use(cors());
-server.use(express.json());
-
-server.get('/clientes', ClienteController.todos);
-
-server.get('/clientes', async (req, res) => {
-    const clientes = await Cliente.listarClientes();
-    res.json(clientes);
-});
-
-server.get('/carros', CarroController.todos);
-
-server.get('/carros', async (req, res) => {
-    const carros = await Carro.listarCarros();
-    res.json(carros);
-});
-
-server.get('/pedidos', PedidoVendaController.todos);
-
-server.get('/pedidos', async (req, res) => {
-    const pedidos = await PedidoVenda.listarPedidos();
-    res.json(pedidos);
-});
-
-new DatabaseModel().testeConexao().then((resdb) =>{
-    if(resdb) {
+new DatabaseModel().testeConexao().then((resdb) => {
+    if (resdb) {
         console.log("Conexão com banco de dados realizada com sucesso!");
         //iniciando o servidor
         server.listen(port, () => {
             console.log(`Servidor iniciado no endereço http://localhost:${port}`);
-});
+        });
     } else {
         console.log("Erro ao conectar com o banco de dados");
     }
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 import { Request, Response, Router } from "express";
+import ClienteController from "./controller/ClienteController";
+import CarroController from "./controller/CarroController";
+import PedidoVendaController from "./controller/PedidoVendaController";
 
 //Cria um roteador
 const router = Router();
@@ -7,6 +10,14 @@ const router = Router();
 router.get("/", (req: Request, res: Response)=> {
     res.json({ mensagem: "Olá, Mundo!"});
 });
+
+// aqui você deve colocar as outras rotas da sua aplicação.
+router.get('/lista/clientes', ClienteController.todos);
+
+router.get('/lista/carros', CarroController.todos);
+
+router.get('/lista/pedidos', PedidoVendaController.todos);
+
 
 //Exportando as rotas
 export { router };
